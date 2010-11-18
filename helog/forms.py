cc22326e969ac756997140a5f829e64c27025574
brendanarnold <none@none>
2010-11-18 21:33:53 +0000

    Added quick calc in javascript

--- helog/forms.py
+++ helog/forms.py
@@ -1,4 +1,4 @@
-from wtforms import Form, TextField, IntegerField, SelectField
+from wtforms import Form, TextField, IntegerField, SelectField, ValidationError
 from wtforms.validators import Required
 from helog.model import users, cryostats, meters, transport_dewars
 
@@ -6,20 +6,35 @@ def create_choices(vals):
     'Choices argument requires key,val tuples tha are strings'
     return [(str(i), vals[i]) for i in range(len(vals))]
 
+def validate_meter_field(form, field):
+    test_str = field.data.replace('.', '')
+    test_str = test_str.strip()
+    for s in test_str:
+        if s not in ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']:
+            raise ValidationError("Enter reading as #####.#")
+
+def validate_dewar_field(form, field):
+    test_str = field.data.strip()
+    for s in test_str:
+        if s not in ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']:
+            raise ValidationError("Enter an integer amount")
+
 # Form for logging a He transfer
 class HeTransferForm(Form):
     user = SelectField('Name', choices=create_choices(users))
     meter = SelectField('Meter', choices=create_choices(meters))
-    meter_before = IntegerField('Before', \
-        [Required(message='Enter a number with no decimal')])
-    meter_after = IntegerField('After', \
-        [Required(message='Enter a number with no decimal')])
-    transport_dewar = SelectField('Transport Dewar', choices=create_choices(transport_dewars))
-    transport_dewar_before = IntegerField('Before', \
-        [Required(message='Enter a number with no decimal')])
-    transport_dewar_after = IntegerField('After', \
-        [Required(message='Enter a number with no decimal')])
-    cryostat = SelectField('Cryostat', choices=create_choices(cryostats))
+    meter_before = TextField('Before', \
+        [Required(), validate_meter_field])
+    meter_after = TextField('After', \
+        [Required(), validate_meter_field])
+    transport_dewar = SelectField('Transport Dewar', \
+        choices=create_choices(transport_dewars))
+    transport_dewar_before = TextField('Before', \
+        [Required(), validate_dewar_field])
+    transport_dewar_after = TextField('After', \
+        [Required(), validate_dewar_field])
+    cryostat = SelectField('Cryostat', \
+        choices=create_choices(cryostats))
 ##     cryostat_before = IntegerField('Before', \
 ##         [Required(message='Enter a number with no decimal')])
 ##     cryostat_after = IntegerField('After', \
---
+++ helog/static/he_calc.js
@@ -0,0 +1,59 @@
+// Returns true if a string is a simple integer
+function isInteger(s) {
+    return /^\s*\d+\s*$/.exec(s);
+}
+// Calculate the boil off and the amount taken from the transport dewar
+function calc_transfers() {
+    // First to boil off
+    var meter_before = $("#meter_before").val();
+    var meter_after = $("#meter_after").val();
+    meter_before = meter_before.replace('\.', '');
+    meter_after = meter_after.replace('\.', '');
+    var boiled_off = 0.0;
+    if (isInteger(meter_before) && isInteger(meter_after)) {
+        meter_before = parseInt(meter_before);
+        meter_after = parseInt(meter_after);
+        boiled_off = (meter_after - meter_before)  / 0.757;
+        $('#he_boiled_off').html(Math.round(boiled_off));
+    } else {
+        $('#he_boiled_off').html('N/A');
+    }
+    // Now the litres taken
+    var transport_dewar_before = $('#transport_dewar_before').val();
+    var transport_dewar_after = $('#transport_dewar_after').val();
+    var taken = 0.0;
+    if (isInteger(transport_dewar_before) && 
+      isInteger(transport_dewar_after)) {
+        transport_dewar_before = parseInt(transport_dewar_before);
+        transport_dewar_after = parseInt(transport_dewar_after);
+        taken = transport_dewar_before - transport_dewar_after;
+        $('#he_taken').html(Math.round(taken));
+    } else {
+        $('#he_taken').html('N/A');
+    } 
+    // Finally try calculating the amount extra in the cryostat
+    var in_cryostat = 0.0;
+    if (isInteger(transport_dewar_before) && 
+      isInteger(transport_dewar_after) &&
+      isInteger(meter_before) && 
+      isInteger(meter_after)
+    ) {
+        transport_dewar_before = parseInt(transport_dewar_before);
+        transport_dewar_after = parseInt(transport_dewar_after);
+        meter_before = parseInt(meter_before);
+        meter_after = parseInt(meter_after);
+        var taken = transport_dewar_before - transport_dewar_after;
+        var boiled_off = (meter_after - meter_before)  / 0.757;
+        in_cryostat = taken - boiled_off;
+        $('#he_in_cryostat').html(Math.round(in_cryostat));
+    } else {
+        $('#he_in_cryostat').html('N/A');
+    } 
+}
+
+$(document).ready(function() {
+    calc_transfers();
+    $("#log_transfer input").change(function(event) {
+        calc_transfers();
+    });
+});
