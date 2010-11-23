541c5c0260770f7e1e640ae943c18e98de0e2cf0
brendanarnold <none@none>
2010-11-23 20:54:09 +0000

    Now uses a windows style .ini files in separate directory

--- helog/config.py
+++ helog/config.py
@@ -1,11 +1,15 @@
 import os
+from ConfigParser import SafeConfigParser
 
-PREFIX = os.path.dirname(__file__)
-USER_LIST_FILENAME = os.path.join(PREFIX, r'users.txt')
-TRANSPORT_DEWAR_LIST_FILENAME = os.path.join(PREFIX, r'transport_dewars.txt')
-METER_LIST_FILENAME = os.path.join(PREFIX, r'meters.txt')
-CRYOSTAT_LIST_FILENAME = os.path.join(PREFIX, r'cryostats.txt')
-DB_FILENAME = os.path.join(PREFIX, r'helog.sqlite')
+cf = SafeConfigParser()
+cf.read(os.path.join(os.path.dirname(__file__), '..', 'config', 'config.ini'))
 
-SECRET_KEY = 'development'
-DEBUG = True
+deploy = cf.get('main', 'DEPLOY')
+
+SECRET_KEY = cf.get(deploy, 'SECRET_KEY')
+DEBUG = cf.get(deploy, 'DEBUG')
+DB_FILENAME = cf.get(deploy, 'DB_FILENAME')
+USER_LIST_FILENAME = cf.get(deploy, 'USER_LIST_FILENAME')
+TRANSPORT_DEWAR_LIST_FILENAME = cf.get(deploy, 'TRANSPORT_DEWAR_LIST_FILENAME')
+METER_LIST_FILENAME = cf.get(deploy, 'METER_LIST_FILENAME')
+CRYOSTAT_LIST_FILENAME = cf.get(deploy, 'CRYOSTAT_LIST_FILENAME')
